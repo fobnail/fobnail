e0d398c9e5401bd5cdd8dedaac9599675e9eb4d1
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-01-17 16:02:23 +0100

    Update FobnailClient

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,6 +1,7 @@
 use core::cell::RefCell;
 
 use alloc::rc::Rc;
+use coap_lite::{MessageClass, Packet, RequestType, ResponseType};
 use smoltcp::socket::{SocketRef, UdpSocket};
 
 use crate::coap::{CoapClient, Error};
@@ -25,43 +26,137 @@ impl<'a> FobnailClient<'a> {
     pub fn poll(&mut self, socket: SocketRef<'_, UdpSocket>) {
         self.coap_client.poll(socket);
 
-        let mut state = self.state.borrow_mut();
+        let state = &*self.state;
+        let state = &mut *state.borrow_mut();
 
-        match &mut *state {
+        match state {
+            State::Idle => (),
             State::Init {
                 ref mut request_pending,
             } => {
                 if !*request_pending {
-                    let mut request = coap_lite::CoapRequest::new();
-                    request.set_path("/");
-                    request.set_method(coap_lite::RequestType::Get);
-
                     *request_pending = true;
+                    let mut request = coap_lite::CoapRequest::new();
+                    request.set_path("/attest");
+                    request.set_method(RequestType::Fetch);
                     let state = Rc::clone(&self.state);
                     self.coap_client
-                        .queue_request(request, move |result| match result {
-                            Ok(_resp) => {
-                                panic!("CHECKPOINT");
-                            }
-                            Err(Error::Timeout) => {
-                                let mut state = state.borrow_mut();
-                                let State::Init {
-                                    ref mut request_pending,
-                                } = &mut *state;
-                                *request_pending = false;
-                            }
-                            Err(e) => {
-                                error!("Communication with attester failed: {:#?}, retrying", e);
-
-                                let mut state = state.borrow_mut();
-                                let State::Init {
-                                    ref mut request_pending,
-                                } = &mut *state;
-                                *request_pending = false;
-                            }
-                        });
+                        .queue_request(request, move |result| Self::handle_response(result, state));
                 }
             }
+            State::InitDataReceived { data } => {
+                info!("Received response from server: {:x?}", data);
+                *state = State::Idle;
+            }
+        }
+    }
+
+    fn handle_response(result: Result<Packet, Error>, state: Rc<RefCell<State>>) {
+        match result {
+            Ok(resp) => Self::handle_server_response(resp, state),
+            Err(e) => Self::handle_coap_error(e, state),
+        }
+    }
+
+    /// Handles communication errors like timeouts or malformed response packets
+    fn handle_coap_error(error: Error, state: Rc<RefCell<State>>) {
+        let state = &*state;
+        match &mut *state.borrow_mut() {
+            State::Init { request_pending } => {
+                if matches!(error, Error::Timeout) {
+                    // Immediatelly resend packet
+                    *request_pending = false
+                } else {
+                    error!("Communication with attester failed: {:#?}, retrying", error);
+                    *request_pending = false
+                }
+            }
+            // We don't send any requests during these states so we shouldn't
+            // get responses.
+            State::InitDataReceived { .. } | State::Idle => unreachable!(),
+        }
+    }
+
+    /// Handles server error responses - communication with server works and we
+    /// received a valid response, but that response contains an error.
+    fn handle_server_error_response(result: &Packet, state: &Rc<RefCell<State>>) -> bool {
+        match result.header.code {
+            #[rustfmt::skip]
+            MessageClass::Response(r) => match r {
+                // 200 (success codes)
+                ResponseType::Created => return false,
+                ResponseType::Deleted => return false,
+                ResponseType::Valid => return false,
+                ResponseType::Changed => return false,
+                ResponseType::Content => return false,
+                ResponseType::Continue => return false,
+
+                // 400 codes
+                ResponseType::BadRequest => error!("server error: Bad request"),
+                ResponseType::Unauthorized => error!("server error: Unauthorized"),
+                ResponseType::BadOption => error!("server error: Bad option"),
+                ResponseType::Forbidden => error!("server error: Forbidden"),
+                ResponseType::NotFound => error!("server error: Not found"),
+                ResponseType::MethodNotAllowed => error!("server error: Method not allowed"),
+                ResponseType::NotAcceptable => error!("server error: Not acceptable"),
+                ResponseType::Conflict => error!("server error: Conflict"),
+                ResponseType::PreconditionFailed => error!("server error: Precondition failed"),
+                ResponseType::RequestEntityTooLarge => error!("server error: RequestEntityTooLarge"),
+                ResponseType::UnsupportedContentFormat => error!("server error: Unsupported content format"),
+                ResponseType::RequestEntityIncomplete => error!("server error: Request entity incomplete"),
+                ResponseType::UnprocessableEntity => error!("server error: Unprocessable entity"),
+                ResponseType::TooManyRequests => error!("server error: Too many requests"),
+                // 500 codes
+                ResponseType::InternalServerError => error!("server error: Internal server error"),
+                ResponseType::NotImplemented => error!("server error: Not implemented"),
+                ResponseType::BadGateway => error!("server error: Bad gateway"),
+                ResponseType::ServiceUnavailable => error!("server error: Service unavailable"),
+                ResponseType::GatewayTimeout => error!("server error: Gateway timeout"),
+                ResponseType::ProxyingNotSupported => error!("server error: Proxying not supported"),
+                ResponseType::HopLimitReached => error!("server error: Hop limit Reached"),
+
+                ResponseType::UnKnown => error!("unknown server error"),
+            },
+            // CoapClient revokes any packets that are not response packet
+            _ => unreachable!("This packet type should be handled by CoapClient"),
+        }
+
+        let state = &*state;
+        let state = &mut *state.borrow_mut();
+        match state {
+            State::Init { .. } => {
+                error!("Switching into IDLE state due to error");
+                *state = State::Idle;
+            }
+            // We don't send any requests during these states so we shouldn't
+            // get responses.
+            State::InitDataReceived { .. } | State::Idle => unreachable!(),
+        }
+
+        true
+    }
+
+    fn handle_server_response(result: Packet, state: Rc<RefCell<State>>) {
+        if Self::handle_server_error_response(&result, &state) {
+            return;
+        }
+
+        let state = &*state;
+        let state = &mut *state.borrow_mut();
+
+        match state {
+            State::Init { .. } => {
+                if result.header.code == MessageClass::Response(ResponseType::Content) {
+                    *state = State::InitDataReceived {
+                        data: result.payload,
+                    };
+                } else {
+                    error!("Server gave invalid response");
+                }
+            }
+            // We don't send any requests during these states so we shouldn't
+            // get responses.
+            State::InitDataReceived { .. } | State::Idle => unreachable!(),
         }
     }
 }
--- src/client/state.rs
+++ src/client/state.rs
@@ -1,6 +1,14 @@
+use alloc::vec::Vec;
+
 pub enum State {
     /// Repeat hello request until server responds
     Init { request_pending: bool },
+
+    /// State after receiving init data
+    InitDataReceived { data: Vec<u8> },
+
+    /// Idle state
+    Idle,
 }
 
 impl Default for State {
