7644358bcc09667d1f01396e7bff0d04fbbce527
Artur Kowalski <arturkow2000@gmail.com>
2022-02-14 17:25:22 +0100

    wip

--- src/client/crypto.rs
+++ src/client/crypto.rs
@@ -59,3 +59,31 @@ impl Drop for Ed25519Key<'_> {
         (drop_fn)(&self.key_id)
     }
 }
+
+pub struct RsaKey<'a> {
+    pub inner: rsa::RsaPublicKey,
+    phantom: PhantomData<&'a ()>,
+}
+
+impl RsaKey<'_> {
+    pub fn load(n: &[u8], e: u32) -> Result<Self, ()> {
+        match rsa::RsaPublicKey::new(
+            rsa::BigUint::from_bytes_be(n),
+            rsa::BigUint::from_slice(&[e]),
+        ) {
+            Ok(key) => Ok(Self {
+                inner: key,
+                phantom: PhantomData,
+            }),
+            Err(e) => {
+                error!("Invalid RSA key: {}", e);
+                Err(())
+            }
+        }
+    }
+}
+
+pub enum Key<'a> {
+    Ed25519(Ed25519Key<'a>),
+    Rsa(RsaKey<'a>),
+}
