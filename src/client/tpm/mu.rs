410799ec3ccaca494577d29375d27201d340e315
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-03-01 16:58:23 +0100

    Harden AIK verification.
    Previous approach for verifying AIK had was not secure, an attacker
    could replace AIK because we were not checking whether received AIK
    matches with it's name used for credential activation. Now, instead of
    relying on AIK name provided from atteter we compute it ourselves. If
    AIK has been tampered with, hashes will be different and credential
    activation will fail.
    Another advantage is that we can confirm that the received key has
    proper attributes set, as required for AIK keys.

--- src/client/tpm/mu.rs
+++ src/client/tpm/mu.rs
@@ -1,11 +1,124 @@
 use alloc::vec::Vec;
 
+macro_rules! decode {
+    ($cursor:expr, $data:expr, [u8]) => {{
+        let array_len = decode!($cursor, $data, u16);
+
+        if array_len > 0 {
+            if let Some(data) = $data.get($cursor..$cursor + array_len as usize) {
+                #[allow(unused_assignments)]
+                {
+                    $cursor += array_len as usize;
+                }
+                data
+            } else {
+                error!("Data truncated");
+                return Err(());
+            }
+        } else {
+            &[]
+        }
+    }};
+
+    ($cursor:expr, $data:expr, $t:ty) => {{
+        let s = ::core::mem::size_of::<$t>();
+        if let Some(data) = $data.get($cursor..$cursor + s) {
+            #[allow(unused_assignments)]
+            {
+                $cursor += s;
+            }
+            <$t>::from_be_bytes(data.try_into().unwrap())
+        } else {
+            error!("Data truncated");
+            return Err(());
+        }
+    }};
+}
+
+macro_rules! ensure {
+    ($x:expr, $($msg:tt)+) => {{
+        if !($x) {
+            error!($($msg)+);
+            return Err(());
+        }
+    }};
+}
+
 /// Corresponds to TPM2_ALG_ID type. Definitions must match with TPM2_ALG_*
 /// constants from tss2_tpm2_types.h
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u16)]
 pub enum Algorithm {
+    Invalid = 0x0,
+    Rsa = 0x1,
     Sha256 = 0xb,
+    Null = 0x10,
+    RsaSsa = 0x14,
+    RsaPss = 0x16,
+}
+
+impl Algorithm {
+    pub fn is_null(self) -> bool {
+        self == Algorithm::Null
+    }
+
+    pub fn is_hash(self) -> bool {
+        match self {
+            Self::Sha256 => true,
+            Self::Invalid | Self::Null | Self::Rsa | Self::RsaSsa | Self::RsaPss => false,
+        }
+    }
+
+    pub fn is_asymmetric(self) -> bool {
+        match self {
+            Self::Rsa | Self::RsaPss | Self::RsaSsa => true,
+            Self::Invalid | Self::Null | Self::Sha256 => false,
+        }
+    }
+
+    pub fn try_from_u8(value: u16) -> Result<Self, ()> {
+        Ok(match value {
+            0x0 => Algorithm::Invalid,
+            0x1 => Algorithm::Rsa,
+            0xb => Algorithm::Sha256,
+            0x10 => Algorithm::Null,
+            0x14 => Algorithm::RsaSsa,
+            0x16 => Algorithm::RsaPss,
+            _ => {
+                error!("Unknown/unsupported algorithm ID 0x{:02x}", value);
+                return Err(());
+            }
+        })
+    }
+
+    pub fn new_asymmetric(value: u16) -> Result<Self, ()> {
+        let this = Self::try_from_u8(value)?;
+        if !this.is_asymmetric() {
+            error!(
+                "Algorithm ID 0x{:02x} is not a supported asymmetric algorithm",
+                value
+            );
+            return Err(());
+        }
+        Ok(this)
+    }
+
+    pub fn new_hash(value: u16) -> Result<Self, ()> {
+        let this = Self::try_from_u8(value)?;
+        if !this.is_hash() {
+            error!(
+                "Algorithm ID 0x{:02x} is not a supported hash algorithm",
+                value
+            );
+            return Err(());
+        }
+        Ok(this)
+    }
+
+    #[inline(always)]
+    pub fn as_raw(self) -> u16 {
+        self as u16
+    }
 }
 
 pub struct LoadedKeyName<'a> {
@@ -104,5 +217,84 @@ impl<'a> IDObject<'a> {
         id_object.extend_from_slice(&self.enc_identity);
         assert_eq!(id_object.len(), id_object_size);
         id_object
+    }
+}
+
+pub enum PublicKey<'a> {
+    Rsa { modulus: &'a [u8], exponent: u32 },
+}
+
+pub struct Public<'a> {
+    pub raw_data: &'a [u8],
+    pub hash_algorithm: Algorithm,
+    pub object_attributes: u32,
+    pub key: PublicKey<'a>,
+}
+
+impl<'a> Public<'a> {
+    pub fn decode(data: &'a [u8]) -> Result<Self, ()> {
+        let mut cursor = 0;
+        let len = decode!(cursor, data, u16);
+        ensure!(len as usize == data.len() - 2, "Invalid TPM2B_PUBLIC size");
+        let algorithm = Algorithm::new_asymmetric(decode!(cursor, data, u16))?;
+        let hash_algorithm = Algorithm::new_hash(decode!(cursor, data, u16))?;
+        let object_attributes = decode!(cursor, data, u32);
+        let auth_policy = decode!(cursor, data, [u8]);
+        ensure!(auth_policy.len() == 0, "authPolicy is not supported");
+
+        // Decode TPMU_PUBLIC_PARMS, contents depend on algorithm type
+        match algorithm {
+            Algorithm::Rsa => {
+                // Decode TPMS_RSA_PARMS structure
+                // TODO: TPMT_SYM_DEF_OBJECT is absent from TPMS_RSA_PARMS when
+                // decoding AIK, however it may be present depending on flags.
+                // Need to inspect this.
+                // let symmetric_algorithm_id = decode!(cursor, data, u16);
+                // let symmetric_key_bits = decode!(cursor, data, u16);
+                // let symmetric_sym_mode = decode!(cursor, data, u16);
+
+                let rsa_scheme = Algorithm::try_from_u8(decode!(cursor, data, u16))?;
+                let rsa_scheme_details = decode!(cursor, data, u16);
+                // TODO: do we need to support this?
+                ensure!(
+                    rsa_scheme.is_null() && rsa_scheme_details == 0x10,
+                    "Non-null scheme, this is not supported"
+                );
+
+                let rsa_key_bits = decode!(cursor, data, u16);
+                ensure!(
+                    rsa_key_bits.is_power_of_two(),
+                    "RSA key size not power-of-two"
+                );
+                let exponent = {
+                    // If exponent is 0 use defaults
+                    let t = decode!(cursor, data, u32);
+                    if t == 0 {
+                        65537
+                    } else {
+                        t
+                    }
+                };
+
+                let modulus = decode!(cursor, data, [u8]);
+                ensure!(
+                    modulus.len() == rsa_key_bits as usize / 8,
+                    "Key size does not match ({} vs {})",
+                    rsa_key_bits / 8,
+                    modulus.len()
+                );
+
+                Ok(Self {
+                    raw_data: data,
+                    object_attributes,
+                    hash_algorithm,
+                    key: PublicKey::Rsa { exponent, modulus },
+                })
+            }
+            _ => {
+                error!("Unsupported asymmetric algorithm {:?}", algorithm);
+                return Err(());
+            }
+        }
     }
 }
