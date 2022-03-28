363ed752c583ddd50da9c61c990758e125397217
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-03-28 15:23:52 +0200

    Drop Ed25519 support
    We don't use Ed25519 anymore as most TPMs don't implement it. Ed25519
    code is dead for a long time already and there is no need to still keep
    it.

--- src/client/signing.rs
+++ src/client/signing.rs
@@ -3,10 +3,7 @@ use core::any::type_name;
 use super::{crypto, proto::SignedObject};
 use rsa::PublicKey as _;
 use serde::Deserialize;
-use trussed::{
-    config::MAX_SIGNATURE_LENGTH,
-    types::{Mechanism, SignatureSerialization},
-};
+use trussed::types::Mechanism;
 
 /// Verifies signature and decodes a CBOR-encoded object if signature is valid.
 pub fn decode_signed_object<'a: 'de, 'de, S, T>(
@@ -24,50 +21,6 @@ where
 
     // We expect SHA256 for RSA and SHA512 for Ed25519
     match signing_key {
-        crypto::Key::Ed25519(key) => {
-            if signed_object.signature.len() > MAX_SIGNATURE_LENGTH {
-                // If verify_ed255() is called with to big signature then Trussed
-                // will panic, so we need to handle that case ourselves.
-                error!("Signature size exceeds MAX_SIGNATURE_LENGTH");
-                return Err(());
-            }
-
-            match trussed::try_syscall!(trussed.verify(
-                Mechanism::Ed255,
-                key.key_id(),
-                signed_object.data,
-                signed_object.signature,
-                SignatureSerialization::Raw
-            )) {
-                Ok(v) if v.valid => {
-                    let sha = trussed::try_syscall!(trussed.hash(
-                        Mechanism::Sha256,
-                        trussed::Bytes::from_slice(signed_object.data).unwrap(),
-                    ))
-                    .map_err(|e| {
-                        error!("Failed to compute SHA-256: {:?}", e);
-                    })?;
-
-                    let inner_object =
-                        trussed::cbor_deserialize::<T>(signed_object.data).map_err(|e| {
-                            error!(
-                                "Failed to deserialize inner object ({}): {}",
-                                type_name::<T>(),
-                                e
-                            )
-                        })?;
-                    Ok((inner_object, signed_object.data, sha.hash))
-                }
-                Ok(_) => {
-                    error!("Signature is invalid");
-                    Err(())
-                }
-                Err(e) => {
-                    error!("verify_ed255() failed: {:?}", e);
-                    Err(())
-                }
-            }
-        }
         crypto::Key::Rsa(key) => {
             let sha = trussed::try_syscall!(trussed.hash(
                 Mechanism::Sha256,
