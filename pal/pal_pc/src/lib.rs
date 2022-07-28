a8c0360a0cbf276d37c22feec2b4c3ca79c7c1bc
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-07-28 12:34:06 +0200

    pal_pc: re-implement network driver
    We are no longer using smoltcp directly, instead we use embassy wrapper
    over smoltcp which provides async API. smoltcp and embassy device APIs
    differ significantly.

--- pal/pal_pc/src/lib.rs
+++ pal/pal_pc/src/lib.rs
@@ -1,14 +1,17 @@
+#![feature(type_alias_impl_trait)]
+
 use std::thread::yield_now;
 
 pub mod button;
 mod device_id;
-pub mod ethernet;
 pub mod led;
+pub mod net;
 pub mod timer;
 pub mod trussed;
 
 pub use device_id::*;
 pub use embassy;
+pub use embassy_net;
 pub use pal_macros::*;
 
 use embassy::executor::Spawner;
@@ -18,8 +21,10 @@ pub fn cpu_relax() {
     yield_now()
 }
 
-pub fn init(_spawner: Spawner) {
+pub fn init(spawner: Spawner) {
     pretty_env_logger::init_custom_env("FOBNAIL_LOG");
     timer::init();
     device_id::init();
+
+    net::init(spawner);
 }
