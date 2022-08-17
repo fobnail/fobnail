b7f90ff2e6cac5dcff0b11f8081c349f3f9fd1c7
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-08-17 09:04:41 +0200

    pal: make Trussed Send
    Currently we are running on a single thread (no interrupt-based task
    switching occurs, only cooperative scheduling) but many APIs require
    Send anyway due to the possibility to spawn future onto separate thread.
    Our futures also must be Send, whether we spawn them or not.

--- pal/pal_nrf/src/trussed/mod.rs
+++ pal/pal_nrf/src/trussed/mod.rs
@@ -1,7 +1,7 @@
-use alloc::rc::Rc;
+use alloc::sync::Arc;
 use alloc::vec::Vec;
-use core::cell::RefCell;
 use cortex_m::peripheral::SCB;
+use embassy_util::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
 
 use hal::pac::NVMC;
 use littlefs2::path::PathBuf;
@@ -24,12 +24,16 @@ impl trussed::platform::UserInterface for UserInterface {
 }
 
 pub struct Syscall {
-    service: Rc<RefCell<Service<Platform>>>,
+    service: Arc<Mutex<CriticalSectionRawMutex, Service<Platform>>>,
 }
 impl trussed::platform::Syscall for Syscall {
     fn syscall(&mut self) {
-        let mut service = self.service.borrow_mut();
-        service.process();
+        loop {
+            if let Ok(mut service) = self.service.try_lock() {
+                service.process();
+                break;
+            }
+        }
     }
 }
 
@@ -51,11 +55,11 @@ pub fn init(client_names: &[&str]) -> Vec<ClientImplementation<Syscall>> {
     let store = unsafe { STORE.take().unwrap() };
     let ui = UserInterface;
     let platform = Platform::new(rng, store, ui);
-    let service = Rc::new(RefCell::new(Service::new(platform)));
+    let service = Arc::new(Mutex::new(Service::new(platform)));
 
     let mut clients = Vec::with_capacity(client_names.len());
     for &client_name in client_names {
-        let client = create_client(Rc::clone(&service), client_name);
+        let client = create_client(Arc::clone(&service), client_name);
         clients.push(client);
     }
 
@@ -63,26 +67,33 @@ pub fn init(client_names: &[&str]) -> Vec<ClientImplementation<Syscall>> {
 }
 
 fn create_client(
-    service: Rc<RefCell<Service<Platform>>>,
+    service: Arc<Mutex<CriticalSectionRawMutex, Service<Platform>>>,
     client_name: &str,
 ) -> ClientImplementation<Syscall> {
-    let mut service_borrowed = service.borrow_mut();
+    let mut requester = None;
 
-    if let Some((req, res)) = Interchange::claim() {
-        service_borrowed
-            .add_endpoint(res, PathBuf::from(client_name.as_bytes()))
-            // add_endpoint on error returns back responder which does not support
-            // Debug trait so we need to drop it in order to use expect()
-            .map_err(|_| ())
-            .expect("add_endpoint() failed (out of resources)");
+    loop {
+        if let Ok(mut locked) = service.try_lock() {
+            if let Some((req, res)) = Interchange::claim() {
+                locked
+                    .add_endpoint(res, PathBuf::from(client_name.as_bytes()))
+                    // add_endpoint on error returns back responder which does not support
+                    // Debug trait so we need to drop it in order to use expect()
+                    .map_err(|_| ())
+                    .expect("add_endpoint() failed (out of resources)");
 
-        drop(service_borrowed);
-        ClientImplementation::new(req, Syscall { service })
-    } else {
-        panic!(
-            "Out of resource when creating Trussed client ({})",
-            client_name
-        );
+                requester = Some(req);
+            } else {
+                panic!(
+                    "Out of resource when creating Trussed client ({})",
+                    client_name
+                );
+            }
+        }
+
+        if let Some(requester) = requester {
+            return ClientImplementation::new(requester, Syscall { service });
+        }
     }
 }
 
