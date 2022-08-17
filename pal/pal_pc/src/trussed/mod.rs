9c0995509a393e46cac999e39b751b234cd6a6ae
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-08-17 09:04:41 +0200

    pal: make Trussed Send
    Currently we are running on a single thread (no interrupt-based task
    switching occurs, only cooperative scheduling) but many APIs require
    Send anyway due to the possibility to spawn future onto separate thread.
    Our futures also must be Send, whether we spawn them or not.

--- pal/pal_pc/src/trussed/mod.rs
+++ pal/pal_pc/src/trussed/mod.rs
@@ -1,5 +1,4 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 use littlefs2::path::PathBuf;
 use trussed::{service::SeedableRng, ClientImplementation, Interchange, Service};
@@ -20,11 +19,11 @@ impl trussed::platform::UserInterface for UserInterface {
 }
 
 pub struct Syscall {
-    service: Rc<RefCell<Service<Platform>>>,
+    service: Arc<Mutex<Service<Platform>>>,
 }
 impl trussed::platform::Syscall for Syscall {
     fn syscall(&mut self) {
-        let mut service = self.service.borrow_mut();
+        let mut service = self.service.lock().unwrap();
         service.process();
     }
 }
@@ -35,11 +34,11 @@ pub fn init(client_names: &[&str]) -> Vec<ClientImplementation<Syscall>> {
     let store = store::init();
     let ui = UserInterface;
     let platform = Platform::new(rng, store, ui);
-    let service = Rc::new(RefCell::new(Service::new(platform)));
+    let service = Arc::new(Mutex::new(Service::new(platform)));
 
     let mut clients = Vec::with_capacity(client_names.len());
     for &client_name in client_names {
-        let client = create_client(Rc::clone(&service), client_name);
+        let client = create_client(Arc::clone(&service), client_name);
         clients.push(client);
     }
 
@@ -47,20 +46,20 @@ pub fn init(client_names: &[&str]) -> Vec<ClientImplementation<Syscall>> {
 }
 
 fn create_client(
-    service: Rc<RefCell<Service<Platform>>>,
+    service: Arc<Mutex<Service<Platform>>>,
     client_name: &str,
 ) -> ClientImplementation<Syscall> {
-    let mut service_borrowed = service.borrow_mut();
+    let mut locked = service.lock().unwrap();
 
     if let Some((req, res)) = Interchange::claim() {
-        service_borrowed
+        locked
             .add_endpoint(res, PathBuf::from(client_name.as_bytes()))
             // add_endpoint on error returns back responder which does not support
             // Debug trait so we need to drop it in order to use expect()
             .map_err(|_| ())
             .expect("add_endpoint() failed (out of resources)");
 
-        drop(service_borrowed);
+        drop(locked);
         ClientImplementation::new(req, Syscall { service })
     } else {
         panic!(
