7604cc739a8a89c05f19ba968443d4f577832602
Artur Kowalski <artur.kowalski@3mdeb.com>
2022-09-01 12:21:22 +0200

    Add heap statistics

--- pal/pal_nrf/src/heap.rs
+++ pal/pal_nrf/src/heap.rs
@@ -7,8 +7,28 @@ use core::{
 use cortex_m::interrupt::{free, Mutex};
 use linked_list_allocator::Heap;
 
+#[cfg(feature = "heap_debug")]
+struct Statistics {
+    biggest_allocation: usize,
+    smallest_allocation: usize,
+    peak_usage: usize,
+}
+
+#[cfg(feature = "heap_debug")]
+impl Statistics {
+    const fn new() -> Self {
+        Self {
+            biggest_allocation: usize::MIN,
+            smallest_allocation: usize::MAX,
+            peak_usage: 0,
+        }
+    }
+}
+
 struct Allocator {
     heap: Mutex<RefCell<Heap>>,
+    #[cfg(feature = "heap_debug")]
+    stats: Mutex<RefCell<Statistics>>,
 }
 
 impl Allocator {
@@ -26,7 +46,21 @@ unsafe impl GlobalAlloc for Allocator {
 
             #[cfg(feature = "heap_debug")]
             {
-                trace!("alloc {} bytes ({} free)", layout.size(), heap.free());
+                let mut stats = self.stats.borrow(cs).borrow_mut();
+                if layout.size() > stats.biggest_allocation {
+                    stats.biggest_allocation = layout.size();
+                }
+
+                if layout.size() < stats.smallest_allocation {
+                    stats.smallest_allocation = layout.size();
+                }
+
+                trace!(
+                    "alloc {} bytes ({} free, {} used)",
+                    layout.size(),
+                    heap.free(),
+                    heap.used()
+                );
             }
 
             let p = heap
@@ -35,7 +69,26 @@ unsafe impl GlobalAlloc for Allocator {
 
             if p.is_null() {
                 error!("Failed to allocate object of size {}", layout.size());
+
+                #[cfg(feature = "heap_debug")]
+                {
+                    let stats = self.stats.borrow(cs).borrow();
+                    debug!("Heap statistics:");
+                    debug!("smallest allocation: {} bytes", stats.smallest_allocation);
+                    debug!("biggest allocation:  {} bytes", stats.biggest_allocation);
+                    debug!("peak usage:          {} bytes", stats.peak_usage);
+                }
             }
+
+            #[cfg(feature = "heap_debug")]
+            {
+                let mut stats = self.stats.borrow(cs).borrow_mut();
+                let usage = heap.used();
+                if usage > stats.peak_usage {
+                    stats.peak_usage = usage;
+                }
+            }
+
             p
         })
     }
@@ -45,7 +98,12 @@ unsafe impl GlobalAlloc for Allocator {
             let mut heap = self.heap.borrow(cs).borrow_mut();
             #[cfg(feature = "heap_debug")]
             {
-                trace!("free {} bytes ({} free)", layout.size(), heap.free());
+                trace!(
+                    "free {} bytes ({} free, {} used)",
+                    layout.size(),
+                    heap.free(),
+                    heap.used()
+                );
             }
 
             heap.deallocate(NonNull::new_unchecked(ptr), layout);
@@ -56,6 +114,8 @@ unsafe impl GlobalAlloc for Allocator {
 #[global_allocator]
 static ALLOCATOR: Allocator = Allocator {
     heap: Mutex::new(RefCell::new(Heap::empty())),
+    #[cfg(feature = "heap_debug")]
+    stats: Mutex::new(RefCell::new(Statistics::new())),
 };
 
 #[alloc_error_handler]
@@ -69,7 +129,7 @@ fn oom_handler(layout: Layout) -> ! {
 
 pub fn init() {
     const RAM_TOP: usize = 0x20040000;
-    const HEAP_SIZE: usize = 65536;
+    const HEAP_SIZE: usize = 65536 + 103000;
 
     let base = cortex_m_rt::heap_start() as usize;
     let end = base + HEAP_SIZE - 1;
